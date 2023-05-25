23710b6383e7092f590b9af7f0c63a21201e598f
Kenny <kenny.ludovic@efrei.net>
2023-05-25 15:35:11 +0200

    Commit version 2.0

---
+++ main.py
@@ -0,0 +1,33 @@
+from functions import *
+
+def menu():
+    print("\n==== Programming Quotes ====")
+    print("1. Random quote")
+    print("2. Display quotes")
+    print("3. Add quote")
+    print ("4. Exit ")
+
+
+def main():
+    while True:
+        quotes = load_quotes("quotes.txt")
+        menu()
+
+        choice = input(">> ")
+
+        if choice == "1":
+            print_quote(random_quote(quotes))
+        elif choice == "2":
+            count = int(input("Enter the number of quotes to display: "))
+            display_quotes(quotes, count)
+        elif choice == "3":
+            add_quote(quotes, "quotes.txt")
+        elif choice == "4":
+            print("Good bye...")
+            break
+        else:
+            print("Invalid input")
+
+
+if __name__ == "__main__":
+    main()
